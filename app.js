895b6ee7ef220cfe76530af32c93d7754d7cdc0a
qunsheng <qunsheng_chen@hotmail.com>
2014-03-02 00:01:56 -0500

    more detailed comments

--- app.js
+++ app.js
@@ -3,16 +3,29 @@
  * An Express server exposes the RESTful API
  * 
  **************************************************************/ 
+// import global packages: express and http module
 var express = require('express'), http = require('http');
 
+// import local module
 var User = require('./model.js');
 
+// express is callable
 var app = express();
 
+// set port
 app.set('port', process.env.PORT || 3000);
+
+// use middleware bodyParser.
+// middleware is a special function.
+// whenever a request come in, it goes to middleware.
+// middleware receives the request, and manipulate the 
+// request and response objects
+// and pass to next function.
 app.use(express.bodyParser());
  
 // Returns an array of user objects
+// no need to pass the next parameter
+// because it will be the end point
 var listUsers = function (req, res) {
 	
 	User.find({}, function(error, dbUsers){
@@ -29,6 +42,8 @@ var listUsers = function (req, res) {
 };
 
 // Returns a single user object
+//no need to pass the next parameter
+//because it will be the end point
 var userDetails = function (req, res) {
 	// query by id
 	User.findById(req.params.id, function(error, user){
@@ -43,6 +58,8 @@ var userDetails = function (req, res) {
 };
 
 // Deletes the given user from the server
+//no need to pass the next parameter
+//because it will be the end point
 var deleteUser = function (req, res) {
 	//remove by id
 	User.findByIdAndRemove( req.params.id, function(error, user){
@@ -57,6 +74,8 @@ var deleteUser = function (req, res) {
 };
 
 // Creates a user based off the payload and returns the new user object 
+//no need to pass the next parameter
+//because it will be the end point
 var createUser = function (req, res) {
 
 	var user = new User();
@@ -78,6 +97,8 @@ var createUser = function (req, res) {
 };
 
 // Updates the given user with the given payload and returns the newly updated user object
+//no need to pass the next parameter
+//because it will be the end point
 var updateUser = function (req, res) {
 
 	var covUser = {
@@ -106,14 +127,19 @@ var updateUser = function (req, res) {
 };
 
 // fill in common headers
+// need pass the next parameter
+// because it is not the end point
 app.all('*', function(req, res, next){
 	  res.header("Access-Control-Allow-Origin", "*");
 	  res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	  res.header("Content-Type", "application/json");
+	  // pass to next step
 	  next();
 });
 
 // correct response options
+//no need to pass the next parameter
+//because it will be the end point
 app.options('*', function(req, res) {
     console.log('!OPTIONS');
     var headers = {};
@@ -121,44 +147,61 @@ app.options('*', function(req, res) {
     headers["Access-Control-Allow-Credentials"] = false;
     headers["Access-Control-Max-Age"] = '86400'; // 24 hours
     res.writeHead(200, headers);
+    // it is end point, no need to call next step
     res.end();
 });
 
 /************************************************************** 
+ * Http get request:
  * GET /users - Returns an array of user objects e.g.
  * [{ age: 38,lastname: 'Chen',firstname: 'Quentin',
  *   _id: 52dfee3b03284ba8140ce3e9 }]
  **************************************************************/ 
+// register the way how express handle http get for string /users
 app.get('/users', listUsers);
 
 /************************************************************** 
+ * Http get request:
  * GET /users/:id - _Returns a single user object e.g. 
  * { age: 38,lastname: 'Chen',firstname: 'Quentin',
- *   _id: 52dfee3b03284ba8140ce3e9 }
+ *   _id: 52dfee3b03284ba8140ce3e9 }.
+ * A word preceded by colon :id means the :id can be replaced 
+ * by any word and become a variable I can access to.
  **************************************************************/ 
+//register the way how express handle http get for string /users/*
 app.get('/users/:id', userDetails);
 
 /************************************************************** 
+ * Http delete request:
  * DELETE /users/:id - Deletes the given user from the server
+ * A word preceded by colon :id means the :id can be replaced 
+ * by any word and become a variable I can access to.
  **************************************************************/ 
+//register the way how express handle http delete for string /users/*
 app.del('/users/:id', deleteUser);
 
 /************************************************************** 
+ * Http post request:
  * POST /users - Creates a user based off the payload
  * and returns the new user object e.g. 
- * { age: 38,lastname: 'Chen',firstname: 'Quentin',
+ * { age: 38, lastname: 'Chen',firstname: 'Quentin',
  *   _id: 52dfee3b03284ba8140ce3e9 }
  **************************************************************/ 
+//register the way how express handle http post for string /users
 app.post('/users', createUser);
 
 /************************************************************** 
+ * Http put request:
  * PUT /users/:id - Updates the given user 
  * with the given payload 
- * and returns the newly updated user object
+ * and returns the newly updated user object.
+ * A word preceded by colon :id means the :id can be replaced 
+ * by any word and become a variable I can access to.
  **************************************************************/ 
+//register the way how express handle http put for string /users/*
 app.put('/users/:id', updateUser);
 
-
+// create web server and listen on port
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
